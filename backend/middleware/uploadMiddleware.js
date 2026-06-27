8cd5c2e55593f54de7f1987e040e38555d6192ba
Moinak Chandra Maiti <moinakmaiti05@gmail.com>
2026-06-27 10:18:35 +0530

    Fix uploads directory

--- backend/middleware/uploadMiddleware.js
+++ backend/middleware/uploadMiddleware.js
@@ -1,20 +1,22 @@
 const multer = require("multer");
+const fs = require("fs");
+const path = require("path");
+
+const uploadDir = path.join(__dirname, "../uploads");
+
+// Create uploads folder if it doesn't exist
+if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir, { recursive: true });
+}
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, "uploads/");
+        cb(null, uploadDir);
     },
 
     filename: function (req, file, cb) {
-        cb(
-            null,
-            Date.now() + "-" + file.originalname
-        );
+        cb(null, Date.now() + "-" + file.originalname);
     },
 });
 
-const upload = multer({
-    storage,
-});
-
-module.exports = upload;
+module.exports = multer({ storage });
